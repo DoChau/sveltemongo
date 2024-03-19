5d0e3ff15ce6f94acabd304ab5867eb99e8b513d
DoChau <dntamchau@gmail.com>
2024-03-19 17:15:25 +0700

    reset

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,16 +1,33 @@
 <script lang="ts">
-	import type { PageData } from './$types'
+	import type { PageData } from "./$types";
 	import { goto } from "$app/navigation";
   
   export let data: PageData;
-  $: ({projects} = data);
+  $: ({projects, user} = data);
 
+  let loading = false;
 
- </script>
+  async function logOut() {
+    loading = true;
+
+    const response = await fetch("/api/logOut", {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    loading = false;
+
+    if (response.status === 200) {
+      goto("/");
+    }
+  }
+</script>
 
 <div>
   <h1>Hello</h1>
-  
+  <p>Connected with email {user.email}</p>
   <p><a href="/profile">See your profile</a></p>
   <hr />
   {#if projects != null}
@@ -20,3 +37,4 @@
   {/if}
 </div>
 
+<button on:click={logOut} disabled={loading}>{loading ? "Loading..." : "Log out"}</button>
