7d7b017b74618fb93a74aa313619309f0706ed9e
DoChau <dntamchau@gmail.com>
2024-03-19 18:51:58 +0700

    fxx

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -10,12 +10,12 @@ import type { PageServerLoad } from "./$types";
 import getUser from "$db/getUser";
 
 export const load: PageServerLoad = async () => {
-  const db = database ?? (await clientPromise).db(DATABASE_NAME);
+  const db = (await clientPromise).db(DATABASE_NAME);
   const data = await db
     .collection<Project>("projects")
     .find({})
     .toArray();
   return {
-    projects: data.map(v => ({ ...v, _id: v._id.toString() }))
+    projects: data
   }
 };
